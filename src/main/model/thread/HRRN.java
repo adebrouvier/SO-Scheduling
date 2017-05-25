d26ef94c5489a791cf0d1008a5224242703500f9
Flacu <facundogonzalezf@gmail.com>
2017-05-25 16:46:05 -0300

    Thread Node + Logica de excecute

--- src/main/model/thread/HRRN.java
+++ src/main/model/thread/HRRN.java
@@ -1,11 +1,12 @@
 package main.model.thread;
 
 import java.util.List;
+import java.util.Queue;
 
 public class HRRN implements Algorithm {
     //No nos faltaria agregar una variable de tiempo de espera?
     @Override
-    public UserLevelThread execute(List<UserLevelThread> ults, UserLevelThread runningUlt) {
+    public TNode execute(Queue<UserLevelThread> ults, UserLevelThread runningUlt) {
         return null;
     }
 }
