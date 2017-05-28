ef4593bcb88a9a3e295ca11b68e6d8e5c415ba48
Flacu <facundogonzalezf@gmail.com>
2017-05-28 00:23:48 -0300

    HRRN and SRT, need more testing (needs fix process not blocking)

--- src/main/model/thread/library/SRT.java
+++ src/main/model/thread/library/SRT.java
@@ -9,7 +9,6 @@ public class SRT implements Algorithm {
     @Override
     public UserLevelThread execute(Queue<UserLevelThread> ults, UserLevelThread runningUlt, int core) {
 
-        if(runningUlt == null) {
             UserLevelThread shortest = ults.peek();
             for (UserLevelThread thread : ults) {
                 if (thread.getCurrentBurst().getRemainingTime() < shortest.getCurrentBurst().getRemainingTime()) {
@@ -18,7 +17,7 @@ public class SRT implements Algorithm {
             }
             ults.remove(shortest);
             runningUlt = shortest;
-        }
+
         if(runningUlt != null){
             runningUlt.setState(ThreadState.RUNNING);
             runningUlt.execute(core * (-1)); // el ult cambia su estado interno
